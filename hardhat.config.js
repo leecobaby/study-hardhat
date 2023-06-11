ce314069a3f8a9624ce4247e1a82f3728618d199
Leeco <leeco19@qq.com>
2023-06-11 19:17:42 +0800

    p2 deploy goerli network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
+// 配置gas估算插件
+require('hardhat-gas-reporter')
+
+const { ALCHEMY_API_KEY, PRIVATE_KEYS } = process.env
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.18'
+  solidity: '0.8.9', // solidity的编译版本
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEYS]
+    }
+  },
+  gasReporter: {
+    // 开启gas估算插件 设置人民币和gas费换算，也可以设置为美元 usd
+    enabled: true,
+    currency: 'USD',
+    token: 'ETH',
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY
+  }
 }
